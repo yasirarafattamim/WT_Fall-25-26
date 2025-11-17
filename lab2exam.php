7209331ceac5b22d910ad7be7465e2a320d4f935
MD.YASIR ARAFAT TAMIM <159172893+yasirarafattamim@users.noreply.github.com>
2025-11-17 11:30:15 +0600

    Added lab2exam.php - Student Registration Form

---
+++ lab2exam.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+
+<html>
+
+<head>
+
+    <title>
+
+        JS basic
+
+    </title>
+
+</head>
+
+<body>
+
+   
+
+<h1 id="demo">Welcome to Js Class </h1>
+
+<p> Lets Start the Class Today</p>
+
+ 
+
+ 
+
+<script>
+
+   var name="Robin Hood";
+
+   let number=120;
+
+   var num_2= 270.0;
+
+   const last_name="Khan";
+
+ 
+
+document.write("Hello World");
+
+console.log("Hello Console");
+
+alert("Hello Alert");
+
+ 
+
+//var body=document.getElementById("demo");
+
+//body.innerHTML="GOOD Bye";
+
+ 
+
+console.log("the dt of name: ",typeof name);
+
+console.log("the dt of num1: ",typeof number);
+
+console.log("the dt of num2: ",typeof num_2);
+
+console.log(typeof undefined);
+
+console.log("the dt of Lname: ",typeof last_name);
+
+ 
+
+let sum = number+num_2;
+
+console.log("The sum is : ",sum);
+
+ 
+
+</script>
+
+ 
+
+ 
+
+ 
+
+</body>
+
+ 
+
+</html>
+
+
